50f2dc8b993f259aa690c3356c0054e494d9743d
wesley-santos <53940899+wjsan@users.noreply.github.com>
2025-11-30 16:45:43 -0300

    feat: add SteamGrid module setup in main function

--- bin/retrogate_server.dart
+++ bin/retrogate_server.dart
@@ -2,11 +2,13 @@ import 'package:retrogate_server/application/grpc/grpc_server.dart';
 import 'package:retrogate_server/modules/config/config_module.dart';
 import 'package:retrogate_server/modules/game/game_module.dart';
 import 'package:retrogate_server/modules/shortcut/shortcut_module.dart';
+import 'package:retrogate_server/modules/steam_grid/steam_grid_module.dart';
 
 void main() async {
   setupConfigModule();
   setupGameModule();
   setupShortcutModule();
+  setupSteamGridModule();
   await Future.wait([
     startGrpcServer(),
   ]);
