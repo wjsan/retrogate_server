c05447fac9f2d9eac057c42f83c2da06d77320e1
wesley-santos <53940899+wjsan@users.noreply.github.com>
2025-11-30 16:45:48 -0300

    feat: add SteamGridService to gRPC server

--- lib/application/grpc/grpc_server.dart
+++ lib/application/grpc/grpc_server.dart
@@ -3,16 +3,19 @@ import 'package:retrogate_server/generated/game/proto/v1/game_service.pbgrpc.dar
 import 'package:retrogate_server/core/service_locator.dart';
 import 'package:retrogate_server/generated/config/proto/v1/config_service.pbgrpc.dart';
 import 'package:retrogate_server/generated/shortcut/proto/v1/shortcut_service.pbgrpc.dart';
+import 'package:retrogate_server/generated/steam_grid/proto/v1/steam_grid_service.pbgrpc.dart';
 
 Future<Server> startGrpcServer() async {
   final configService = getIt.get<ConfigServiceBase>();
   final gameService = getIt.get<GameServiceBase>();
   final shortcutService = getIt.get<ShortcutServiceBase>();
+  final steamGridService = getIt.get<SteamGridServiceBase>();
 
   final server = Server.create(services: [
     configService,
     gameService,
     shortcutService,
+    steamGridService
   ]);
 
   await server.serve(port: 5000);
